9d6c2f87622e04b07f42ec95a0be50a31962e8bb
yonatan78 <yonatansuarez78@gmail.com>
2023-05-09 15:53:28 -0500

    agrego proyecto

---
+++ js/import.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    // Get the value of the name input field
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+
+
+    // Check if the name field is empty
+    if (name === "" || email === "" || subject === "" || message === "") {
+        swal("Error", "Complete todos los campos", "error");
+        return;
+    }
+
+    // Show the confirmation dialog
+    swal({
+        title: "Seguro de enviar",
+        text: "Puedes Modificar tu respuesta! Estas a tiempo",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                swal("Tu respuesta ha sido enviada correctamente!", {
+                    icon: "success"
+                });
+
+                // Limpiar los campos
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("subject").value = "";
+                document.getElementById("message").value = "";
+                
+            } else {
+                swal("Modifica o cambia tu respuesta!");
+            }
+        });
+}
