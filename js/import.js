d10adbc74e84466e399084df5fe4b262d0d5dd53
yonatan78 <yonatansuarez78@gmail.com>
2023-07-24 18:56:44 -0500

    b

--- js/import.js
+++ js/import.js
@@ -1,38 +1,26 @@
-function myFunction() {
-    // Get the value of the name input field
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+function confirmAndSubmit() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
 
-
-
-    // Check if the name field is empty
-    if (name === "" || email === "" || message === "") {
-        swal("Error", "Complete todos los campos", "error");
-        return;
-    }
-
+  // Check if the form fields are empty
+  if (name === "" || email === "" || message === "") {
+    swal("Error", "Complete todos los campos", "error");
+  } else {
     // Show the confirmation dialog
     swal({
-        title: "Seguro de enviar",
-        text: "Puedes Modificar tu respuesta! Estas a tiempo",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-    })
-        .then((willDelete) => {
-            if (willDelete) {
-                swal("Tu respuesta ha sido enviada correctamente!", {
-                    icon: "success"
-                });
-
-                // Limpiar los campos
-                document.getElementById("name").value = "";
-                document.getElementById("email").value = "";
-                document.getElementById("message").value = "";
-                
-            } else {
-                swal("Modifica o cambia tu respuesta!");
-            }
-        });
+      title: "Seguro de enviar",
+      text: "Puedes modificar tu respuesta. ¿Estás seguro de enviar el formulario?",
+      icon: "warning",
+      buttons: ["Cancelar", "Enviar"],
+      dangerMode: true,
+    }).then((willSubmit) => {
+      if (willSubmit) {
+        // Submit the form programmatically
+        document.getElementById("contact-form").submit();
+      } else {
+        swal("Modifica o cambia tu respuesta!");
+      }
+    });
+  }
 }
